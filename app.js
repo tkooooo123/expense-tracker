a6208a9bca08022b4bc7143ff37a70abf632e319
tkooooo123 <tkooooo123@gmail.com>
2022-04-03 16:48:46 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
+mongoose.connect('mongodb://localhost/expense-tracker')
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
 app.get('/', (req, res) => {
     res.send('Expense Tracker ')
 })
