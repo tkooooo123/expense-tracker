d39cdd6031fc421241a205903e89657c4bde9911
tkooooo123 <tkooooo123@gmail.com>
2022-04-05 12:08:47 +0800

    feat: modify index page & add new page

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 
 const Record = require('./models/record')
+const Category = require('./models/category')
+const category = require('./models/category')
 
 const app = express()
 const port = 3000
@@ -22,11 +24,56 @@ app.engine('hbs', exphbs.create({ defaultLayout: 'main', extname: '.hbs' }).engi
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    Record.find()
-    .lean()
-    .then(record => res.render('index', { record }))
-    .catch(err => console.log(err))
+    const categoryNames = []
+    const recordDatas = []
+    Category.find()
+        .lean()
+        .then(categories => {
+            categories.filter(category => {
+                categoryNames.push(category.name)
+                //console.log(category._id)
+               //console.log(category.icon)
+            })
+            Record.find()
+                .populate('categoryId')//關聯Category資料庫
+                .lean()
+                .then(records => {
+                    let totalAmount = 0
+                    records.forEach(record => {
+                        //console.log(record.categoryId)
+                        totalAmount += record.amount
+
+                        recordDatas.push({
+                            id: record._id,
+                            name: record.name,
+                            date: record.date,
+                            amount: record.amount,
+                            icon: record.categoryId.icon
+                        })
+                    })
+                    //console.log(recordDatas)
+                    //console.log(categoryNames)
+                    //console.log(totalAmount)
+                    res.render('index', { record: recordDatas, categories: categoryNames, totalAmount})
+                })
+        })
+        .catch(err => console.log(err))
 })
+app.get('/records/new', (req, res) => {
+    const categoryNames = []
+    Category.find()
+    .lean()
+    .then(categories => {
+        categories.filter(category => {
+            categoryNames.push(category.name)
+        })
+        console.log(categoryNames)
+    })
+    .catch(err => console.log(err))
+    
+    res.render('new' , {categories: categoryNames} )
+})
+
 
 app.listen(port, () => {
     console.log('App is running on http://localhost:3000')
