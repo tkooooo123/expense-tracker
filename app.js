235ed798c80fcc6af5f37d74a270d5669b5f4f34
tkooooo123 <tkooooo123@gmail.com>
2022-04-05 19:55:58 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -135,6 +135,13 @@ app.post('/records/:id/edit', (req, res) => {
 
 
 })
+app.post('/records/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
 
 app.listen(port, () => {
     console.log('App is running on http://localhost:3000')
