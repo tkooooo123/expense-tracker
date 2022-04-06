48f3c0d6cde0cbcb86c39802a92645d259895fe5
tkooooo123 <tkooooo123@gmail.com>
2022-04-06 21:15:29 +0800

    feat: add userId data in record model

--- models/record.js
+++ models/record.js
@@ -13,11 +13,17 @@ const recordSchema = new Schema({
         type: Number,
         required: true
     },
-    categoryId: {  
+    categoryId: {
         type: Schema.Types.ObjectId,
         ref: 'Category',
         index: true,
         required: true
-      }
+    },
+    userId: {  
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true,
+        required: true
+    }
 })
 module.exports = mongoose.model('Record', recordSchema)
