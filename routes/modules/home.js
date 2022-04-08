f19e841ce116c01d2dbf82df9ba62e626f28c1bc
tkooooo123 <tkooooo123@gmail.com>
2022-04-09 02:07:22 +0800

    feat: add filter function by category and modify homepage router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,40 +5,53 @@ const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
     const userId = req.user._id
-    const categoryNames = []
-    const recordDatas = []
     Category.find()
         .lean()
         .then(categories => {
             categories.filter(category => {
-                categoryNames.push(category.name)
-                //console.log(category._id)
-                //console.log(category.icon)
             })
-            Record.find({userId})
+            Record.find({ userId })
                 .populate('categoryId')//關聯Category資料庫
                 .lean()
                 .then(records => {
                     let totalAmount = 0
                     records.forEach(record => {
-                        //console.log(record.categoryId)
                         totalAmount += record.amount
-
-                        recordDatas.push({
-                            id: record._id,
-                            name: record.name,
-                            date: record.date,
-                            amount: record.amount,
-                            icon: record.categoryId.icon
-                        })
                     })
-                    //console.log(recordDatas)
-                    //console.log(categoryNames)
-                    //console.log(totalAmount)
-                    res.render('index', { record: recordDatas, categories: categoryNames, totalAmount })
+                    res.render('index', { records, categories, totalAmount })
                 })
         })
         .catch(err => console.log(err))
 })
+
+router.post('/', (req, res) => {
+    const userId = req.user._id
+    const { selectedCategory } = req.body
+    if (selectedCategory) {
+        Category.find({ _id: { $ne: selectedCategory } })
+        .lean()
+        .then(notSelectedCategories => {
+            Category.findById(selectedCategory)
+                .lean()
+                .then(selectedCategory => {
+                    console.log(selectedCategory.id)
+                    Record.find({ userId, categoryId: selectedCategory })
+                        .populate('categoryId')//關聯Category資料庫
+                        .lean()
+                        .then(records => {
+                            let totalAmount = 0
+                            records.forEach(record => {
+                                //console.log(record.categoryId)
+                                totalAmount += record.amount
+                            })
+                            res.render('index', { records, selectedCategory , notSelectedCategories , totalAmount })
+                        })
+                })
+        })
+    } else {
+        res.redirect('/')
+    }
+})
+
 
 module.exports = router
